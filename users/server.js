c92451c5f54b14f6ad37a06b617426dbd4a34b00
Ha Quynh Nguyen <haquynhnguyen@vpn5-10.f6.fr>
2017-12-06 19:11:35 +0700

    04: 026 - NonNull Fields and Mutiations

--- users/server.js
+++ users/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
+
+// graphiql is a dev tool to make query in dev server
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}));
 
 app.listen(4000, () => {
     console.log('listening');
